13a4e7765e99c9986f83a0a33909c3f8aaef4358
2A Modular Info <2ainfo@basilides.local>
2019-08-20 15:03:01 -0300

    20/08

--- siteEmpresa/script.js
+++ siteEmpresa/script.js
@@ -1,7 +1,9 @@
-document.querySelectorAll('.pergunta').forEach((e)=>{
-    console.log(e)
+document.querySelectorAll('.pergunta').forEach(e => e.childNodes[1].innerHTML += "<span>+</span>")
+
+document.querySelectorAll('.pergunta').forEach(e=>{
     e.addEventListener('click', () => {
         e.childNodes[1].classList.toggle('color')
+        e.childNodes[1].childNodes[1].classList.toggle('rotate')
         e.classList.toggle('maxHeight')
     })
 })
