46e28e45b184d3fc3256ae82e15ba45ce05a23a1
Thiago <thiago.silva972@etec.sp.gov.br>
2019-08-20 20:32:24 -0300

    20/08

--- siteEmpresa/script.js
+++ siteEmpresa/script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.pergunta').forEach(e => e.childNodes[1].innerHTML += "<span>+</span>")
 
-document.querySelectorAll('.pergunta').forEach(e=>{
+document.querySelectorAll('.pergunta').forEach(e => {
     e.addEventListener('click', () => {
         e.childNodes[1].classList.toggle('color')
         e.childNodes[1].childNodes[1].classList.toggle('rotate')
