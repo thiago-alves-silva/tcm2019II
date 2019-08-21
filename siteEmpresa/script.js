7c96ba0e5e4457bc2399cca0490bccd82787f977
Thiago <thiago.silva972@etec.sp.gov.br>
2019-08-20 21:44:35 -0300

    20/08

--- siteEmpresa/script.js
+++ siteEmpresa/script.js
@@ -8,12 +8,23 @@ document.querySelectorAll('.pergunta').forEach(e => {
     })
 })
 
-const items = document.querySelectorAll('[data-animation]')
 function animeScroll() {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    items.forEach((e, i) => {
+    document.querySelectorAll('[data-animation]').forEach((e, i) => {
         if(windowTop > e.offsetTop) setInterval(() => e.classList.add('animate'), 50*i)
     })
 }
 animeScroll()
 window.addEventListener('scroll', () => animeScroll())
+
+let a = 0
+function animeScroll2(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    if(a==0){
+        document.querySelectorAll('[data-icon_contato]').forEach((e,i) => {
+        if((windowTop > e.offsetTop)) 
+        { setInterval(() => e.classList.add('animate2'), 300*i); a=1 }
+        })
+    }
+}
+window.addEventListener('scroll', () => animeScroll2())
