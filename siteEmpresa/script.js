6a84b57ada183d998efb1bab9471e39b5edbcf95
Thiago <thiago.silva972@etec.sp.gov.br>
2019-08-29 22:23:24 -0300

    29/08

--- siteEmpresa/script.js
+++ siteEmpresa/script.js
@@ -34,6 +34,32 @@ function animeScroll2(){
 }
 window.addEventListener('scroll', () => animeScroll2())
 
+function animeScroll3() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    document.querySelectorAll('[data-equipe]').forEach((e, i) => {
+        if(windowTop > e.offsetTop) setInterval(() => e.classList.add('animate3'), 100*i)
+    })
+}
+animeScroll3()
+window.addEventListener('scroll', () => animeScroll3())
+
+const imgs = document.querySelectorAll('.equipe')
+const imgsHover = document.querySelectorAll('.hover')
+const divs = document.querySelectorAll('div#equipe > div')
+function equipeImg(){
+    divs.forEach((e, i) => {
+        e.addEventListener('mouseover', () => {
+            imgs[i].style.transform = 'scale(1.3)'
+            imgsHover[i].style.opacity = '1'
+        })
+        e.addEventListener('mouseout', () => {
+            imgs[i].style.transform = 'scale(1)'
+            imgsHover[i].style.opacity = '0'
+        })
+    })
+}
+equipeImg()
+
 writter()
 function writter() {
 let txt = document.getElementById('titulo').innerHTML.split("");
