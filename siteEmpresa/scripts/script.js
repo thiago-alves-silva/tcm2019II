a36d8200dca7a0a20731e93627e08b241397597d
Thiago <thiago.silva972@etec.sp.gov.br>
2019-09-14 16:44:16 -0300

    14/09

--- siteEmpresa/scripts/script.js
+++ siteEmpresa/scripts/script.js
@@ -1,3 +1,5 @@
+const debounce3=function(n,t,e){let o;return function(...u){const c=this,i=e&&!o;clearTimeout(o),o=setTimeout(function(){o=null,e||n.apply(c,u)},t),i&&n.apply(c,u)}};
+
 document.querySelectorAll('.pergunta').forEach(e => e.childNodes[1].innerHTML += "<span>+</span>")
 
 document.querySelectorAll('.pergunta').forEach(e => {
@@ -8,3 +10,41 @@ document.querySelectorAll('.pergunta').forEach(e => {
         e.classList.toggle('maxHeight')
     })
 })
+
+document.querySelectorAll('div#menu-itens ul li').forEach((e, i) => {
+    setTimeout(()=>e.style = 'opacity: 1', 200*i)
+})
+function menuBar(){
+    let height = document.querySelectorAll('.height')
+    const li = document.querySelectorAll('div#menu-itens ul li');
+    const header = document.querySelector('header')
+    const a = window.pageYOffset + header.clientHeight
+    if(window.pageYOffset>=(height[0].offsetTop + height[0].clientHeight)){
+        header.style = 'position: fixed; background-color: #151819;'
+
+        if(a >=height[1].offsetTop && a <=height[2].offsetTop) li[0].style.color = '#ed145b'
+        else li[0].style.color = ''
+
+        if(a >=height[2].offsetTop && a <=height[3].offsetTop) li[1].style.color = '#ed145b'
+        else li[1].style.color = ''
+
+        if(a >=height[3].offsetTop && a <=height[4].offsetTop) li[2].style.color = '#ed145b'
+        else li[2].style.color = ''
+
+        if(a >=height[4].offsetTop && a <=height[5].offsetTop) li[3].style.color = '#ed145b'
+        else li[3].style.color = ''
+
+        if(a >=height[5].offsetTop && a <=height[6].offsetTop) li[4].style.color = '#ed145b'
+        else li[4].style.color = ''
+
+        if(a >=(height[6].offsetTop) && a <=(height[6].offsetTop + height[6].clientHeight)) li[5].style.color = '#ed145b'
+        else li[5].style.color = ''
+    }
+    else{
+        header.style = 'position: absolute'
+        header.style = ''
+        document.querySelector('div#cabecalho > h1').style = ''
+    }
+}
+menuBar()
+window.addEventListener('scroll', debounce3(() => menuBar(), 50))
