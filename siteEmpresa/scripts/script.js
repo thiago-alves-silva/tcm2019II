795c35c1838c7add593c992a1e7d8db5199305bd
Thiago <thiago.silva972@etec.sp.gov.br>
2019-09-14 17:21:34 -0300

    14/09

--- siteEmpresa/scripts/script.js
+++ siteEmpresa/scripts/script.js
@@ -15,36 +15,20 @@ document.querySelectorAll('div#menu-itens ul li').forEach((e, i) => {
     setTimeout(()=>e.style = 'opacity: 1', 200*i)
 })
 function menuBar(){
-    let height = document.querySelectorAll('.height')
+    const height = document.querySelectorAll('.height')
     const li = document.querySelectorAll('div#menu-itens ul li');
     const header = document.querySelector('header')
     const a = window.pageYOffset + header.clientHeight
     if(window.pageYOffset>=(height[0].offsetTop + height[0].clientHeight)){
         header.style = 'position: fixed; background-color: #151819;'
-
-        if(a >=height[1].offsetTop && a <=height[2].offsetTop) li[0].style.color = '#ed145b'
-        else li[0].style.color = ''
-
-        if(a >=height[2].offsetTop && a <=height[3].offsetTop) li[1].style.color = '#ed145b'
-        else li[1].style.color = ''
-
-        if(a >=height[3].offsetTop && a <=height[4].offsetTop) li[2].style.color = '#ed145b'
-        else li[2].style.color = ''
-
-        if(a >=height[4].offsetTop && a <=height[5].offsetTop) li[3].style.color = '#ed145b'
-        else li[3].style.color = ''
-
-        if(a >=height[5].offsetTop && a <=height[6].offsetTop) li[4].style.color = '#ed145b'
-        else li[4].style.color = ''
-
-        if(a >=(height[6].offsetTop) && a <=(height[6].offsetTop + height[6].clientHeight)) li[5].style.color = '#ed145b'
-        else li[5].style.color = ''
+        a>=height[1].offsetTop && a<=height[2].offsetTop?li[0].style.color = '#ed145b':li[0].style.color = ''
+        a>=height[2].offsetTop && a<=height[3].offsetTop?li[1].style.color = '#ed145b':li[1].style.color = ''
+        a>=height[3].offsetTop && a<=height[4].offsetTop?li[2].style.color = '#ed145b':li[2].style.color = ''
+        a>=height[4].offsetTop && a<=height[5].offsetTop?li[3].style.color = '#ed145b':li[3].style.color = ''
+        a>=height[5].offsetTop && a<=height[6].offsetTop?li[4].style.color = '#ed145b':li[4].style.color = ''
+        a>=(height[6].offsetTop) && a <=(height[6].offsetTop + height[6].clientHeight)?li[5].style.color = '#ed145b':li[5].style.color = ''
     }
-    else{
-        header.style = 'position: absolute'
-        header.style = ''
-        document.querySelector('div#cabecalho > h1').style = ''
-    }
+    else header.style = 'position: absolute'
 }
 menuBar()
 window.addEventListener('scroll', debounce3(() => menuBar(), 50))
