9a6907442aecc6061e4abb053a612816633c02fb
Thiago <thiago.silva972@etec.sp.gov.br>
2019-09-15 12:21:28 -0300

    15/09

--- siteEmpresa/scripts/scriptServicos.js
+++ siteEmpresa/scripts/scriptServicos.js
@@ -4,7 +4,11 @@ abrirServico()
 function abrirServico(){
     servicos.forEach((e, i)=>{
         e.addEventListener('click', ()=>{
+            servicos.forEach(e=>e.classList.remove('shadow'))
+            servicos[i].classList.toggle('shadow')
+            mostrarServico.forEach(e=>{e.classList.remove('display'); e.style =''})
             mostrarServico[i].classList.toggle('display')
+            setTimeout(()=>mostrarServico[i].style = 'max-height: 10vw', 1) 
         })
     })
 }
