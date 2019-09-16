cab5bef7d5c5876bdfe7e2f2ac4c64476b4b9b69
Thiago <thiago.silva972@etec.sp.gov.br>
2019-09-15 21:39:08 -0300

    15/09

--- siteEmpresa/scripts/scriptServicos.js
+++ siteEmpresa/scripts/scriptServicos.js
@@ -1,7 +1,7 @@
 const servicos = document.querySelectorAll('div#itens > div')
 const mostrarServico = document.querySelectorAll('div#displayServico > div.serv')
 const h1 = document.querySelector('h1#click')
-const wallpaper = document.querySelector('div.wallpaper')
+const wallpaper = document.querySelector('div#displayServico')
 const texto = document.querySelectorAll('div.serv > div.textoServ')
 
 abrirServico()
@@ -13,9 +13,16 @@ function abrirServico(){
             servicos[i].classList.toggle('shadow')
             mostrarServico.forEach(e=>{e.classList.remove('display'); e.style =''})
             mostrarServico[i].classList.toggle('display')
-            setTimeout(()=>mostrarServico[i].style = 'max-height: 20vw', 1) 
+            switch(i){
+                case 0: wallpaper.style.backgroundImage = 'url("img/servico/system.jpg")'; break;
+                case 1: wallpaper.style.backgroundImage = 'url("img/servico/web.jpg")'; break;
+                case 2: wallpaper.style.backgroundImage = 'url("img/servico/mobile.jpg")'; break;
+                case 3: wallpaper.style.backgroundImage = 'url("img/servico/design.jpg")'; break;
+                case 4: wallpaper.style.backgroundImage = 'url("img/servico/manutencao.jpg")'; break;
+                case 5: wallpaper.style.backgroundImage = 'url("img/servico/analise.jpg")'; break;
+            }
             texto.forEach(e=>e.style = '')
-            setTimeout(()=>texto[i].style = 'opacity: 1', 500)
+            setTimeout(()=>texto[i].style = 'opacity: 1', 200)
         })
     })
 }
