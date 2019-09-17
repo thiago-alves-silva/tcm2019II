5cec955aa8f1e58af88a9f77d1fd41ed864cd003
2A Modular Info <2ainfo@basilides.local>
2019-09-17 14:07:22 -0300

    17/09

--- siteEmpresa/scripts/scriptServicos.js
+++ siteEmpresa/scripts/scriptServicos.js
@@ -6,23 +6,39 @@ const texto = document.querySelectorAll('div.serv > div.textoServ')
 
 abrirServico()
 function abrirServico(){
+    let click, newClick;
     servicos.forEach((e, i)=>{
         e.addEventListener('click', ()=>{
-            if(document.querySelector('h1#click')) document.querySelector('div#displayServico').removeChild(h1)
-            servicos.forEach(e=>e.classList.remove('shadow'))
-            servicos[i].classList.toggle('shadow')
-            mostrarServico.forEach(e=>{e.classList.remove('display'); e.style =''})
-            mostrarServico[i].classList.toggle('display')
-            switch(i){
-                case 0: wallpaper.style.backgroundImage = 'url("img/servico/system.jpg")'; break;
-                case 1: wallpaper.style.backgroundImage = 'url("img/servico/web.jpg")'; break;
-                case 2: wallpaper.style.backgroundImage = 'url("img/servico/mobile.jpg")'; break;
-                case 3: wallpaper.style.backgroundImage = 'url("img/servico/design.jpg")'; break;
-                case 4: wallpaper.style.backgroundImage = 'url("img/servico/manutencao.jpg")'; break;
-                case 5: wallpaper.style.backgroundImage = 'url("img/servico/analise.jpg")'; break;
+            click = newClick
+            newClick = i
+            if(click === newClick){
+                //wallpaper.style = ""
+                //mostrarServico.forEach(e=>{e.classList.remove('display'); e.style =''})
+                servicos.forEach(e=>e.classList.remove('shadow'))
+                wallpaper.style.opacity = '0'
+                mostrarServico[i].classList.remove('display');
+                h1.style.display = 'block'
+                newClick = ''
             }
-            texto.forEach(e=>e.style = '')
-            setTimeout(()=>texto[i].style = 'opacity: 1', 200)
+            else {
+                if(document.querySelector('h1#click')) h1.style.display = 'none'
+                servicos.forEach(e=>e.classList.remove('shadow'))
+                servicos[i].classList.toggle('shadow')
+                mostrarServico.forEach(e=>{e.classList.remove('display'); e.style =''})
+                mostrarServico[i].classList.toggle('display')
+                setTimeout(() =>  wallpaper.style.transition += 'opacity .5s, background-image .5s', 1);
+                switch(i){
+                    case 0: wallpaper.style.backgroundImage = 'url("img/servico/system.jpg")'; break;
+                    case 1: wallpaper.style.backgroundImage = 'url("img/servico/web.jpg")'; break;
+                    case 2: wallpaper.style.backgroundImage = 'url("img/servico/mobile.jpg")'; break;
+                    case 3: wallpaper.style.backgroundImage = 'url("img/servico/design.jpg")'; break;
+                    case 4: wallpaper.style.backgroundImage = 'url("img/servico/manutencao.jpg")'; break;
+                    case 5: wallpaper.style.backgroundImage = 'url("img/servico/analise.jpg")'; break;
+                }
+                setTimeout(()=>wallpaper.style.opacity = '1',100)
+                texto.forEach(e=>e.style = '')
+                setTimeout(()=>texto[i].style = 'opacity: 1', 200)
+            }
         })
     })
 }
