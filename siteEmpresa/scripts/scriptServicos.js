2d42959bafbb3eeafd3e6188dcf9415c89e74bb8
Thiago <thiago.silva972@etec.sp.gov.br>
2019-10-06 13:44:40 -0300

    06/10

--- siteEmpresa/scripts/scriptServicos.js
+++ siteEmpresa/scripts/scriptServicos.js
@@ -1,42 +1,42 @@
 const servicos = document.querySelectorAll('div#itens > div')
 const mostrarServico = document.querySelectorAll('div#displayServico > div.serv')
-const h1 = document.querySelector('h1#click')
-const wallpaper = document.querySelector('div#displayServico')
-const texto = document.querySelectorAll('div.serv > div.textoServ')
+const carrossel = document.querySelector('div#displayServico')
+const pause = document.querySelector('.pause')
+const voltar = document.getElementById('voltar')
+const avancar = document.getElementById('avancar')
+mostrarServico[0].style.backgroundImage = 'url("img/servico/system.jpg")'
+mostrarServico[1].style.backgroundImage = 'url("img/servico/web.jpg")'
+mostrarServico[2].style.backgroundImage = 'url("img/servico/mobile.jpg")'
+mostrarServico[3].style.backgroundImage = 'url("img/servico/design.jpg")'
+mostrarServico[4].style.backgroundImage = 'url("img/servico/manutencao.jpg")'
+mostrarServico[5].style.backgroundImage = 'url("img/servico/analise.jpg")'
 
-abrirServico()
-function abrirServico(){
-    let click, newClick;
-    servicos.forEach((e, i)=>{
-        e.addEventListener('click', ()=>{
-            click = newClick
-            newClick = i
-            if(click === newClick){
-                servicos.forEach(e=>e.classList.remove('shadow'))
-                wallpaper.style.opacity = '0'
-                mostrarServico[i].classList.remove('display');
-                h1.style.display = 'block'
-                newClick = ''
-            }
-            else {
-                if(document.querySelector('h1#click')) h1.style.display = 'none'
-                servicos.forEach(e=>e.classList.remove('shadow'))
-                servicos[i].classList.toggle('shadow')
-                mostrarServico.forEach(e=>{e.classList.remove('display'); e.style =''})
-                mostrarServico[i].classList.toggle('display')
-                setTimeout(() =>  wallpaper.style.transition += 'opacity .5s, background-image .5s', 1);
-                switch(i){
-                    case 0: wallpaper.style.backgroundImage = 'url("img/servico/system.jpg")'; break;
-                    case 1: wallpaper.style.backgroundImage = 'url("img/servico/web.jpg")'; break;
-                    case 2: wallpaper.style.backgroundImage = 'url("img/servico/mobile.jpg")'; break;
-                    case 3: wallpaper.style.backgroundImage = 'url("img/servico/design.jpg")'; break;
-                    case 4: wallpaper.style.backgroundImage = 'url("img/servico/manutencao.jpg")'; break;
-                    case 5: wallpaper.style.backgroundImage = 'url("img/servico/analise.jpg")'; break;
-                }
-                setTimeout(()=>wallpaper.style.opacity = '1',100)
-                texto.forEach(e=>e.style = '')
-                setTimeout(()=>texto[i].style = 'opacity: 1', 200)
-            }
-        })
-    })
+let indice = 0;
+function efeitoCarrossel(){
+    servicos.forEach(e=>e.classList.remove('shadow'))
+    servicos[indice].classList.toggle('shadow')
+    mostrarServico.forEach(e=>e.classList.add('small'))
+    mostrarServico[indice].classList.toggle('small')
+    let final = (document.querySelector('div.serv').clientWidth*100)/window.innerWidth+(window.innerWidth*10)/window.innerWidth
+    switch(indice){
+        case 0: carrossel.style.transform = `translateX(0)`; break;
+        case 1: carrossel.style.transform = `translateX(-${final}vw)`; break;
+        case 2: carrossel.style.transform = `translateX(-${final*2}vw)`; break;
+        case 3: carrossel.style.transform = `translateX(-${final*3}vw)`; break;
+        case 4: carrossel.style.transform = `translateX(-${final*4}vw)`; break;
+        case 5: carrossel.style.transform = `translateX(-${final*5}vw)`; break;
+    }
 }
+efeitoCarrossel()
+let intervalCarrossel = setInterval(()=> {indice<5?indice++:indice=0; efeitoCarrossel()}, 5000)
+
+voltar.addEventListener('click', ()=> {
+    clearInterval(intervalCarrossel)
+    indice>=1?indice--:indice=5;
+    efeitoCarrossel()
+})
+avancar.addEventListener('click', ()=> {
+    clearInterval(intervalCarrossel)
+    indice<5?indice++:indice=0;
+    efeitoCarrossel()
+})
